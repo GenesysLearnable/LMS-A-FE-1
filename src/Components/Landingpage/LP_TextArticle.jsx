ace6082aa906c06f70b6aa81afbb7142f9772baa
Mereson <chimeremnmaojinta@gmail.com>
2024-05-08 19:19:02 +0100

    Changed Landing page naming

--- src/Components/Landingpage/LP_TextArticle.jsx
+++ src/Components/Landingpage/LP_TextArticle.jsx
@@ -1,13 +1,9 @@
 import Button from "./Button"
 
-const HTextArticle = ({ heading, text, btn, title, textStyle }) => {
+const LP_TextArticle = ({ heading, text, btn, title, textStyle }) => {
   return (
     <article className="mt-[61px] flex flex-col justify-between gap-7">
-      <p
-        className={`${title} w-[526px] font-bold leading-[46px]`}
-      >
-        {heading}
-      </p>
+      <p className={`${title} w-[526px] font-bold leading-[46px]`}>{heading}</p>
       <p
         className={`w-[546px] ${textStyle} text-base font-normal leading-normal`}
       >
@@ -19,4 +15,4 @@ const HTextArticle = ({ heading, text, btn, title, textStyle }) => {
   )
 }
 
-export default HTextArticle
+export default LP_TextArticle
--- src/Components/Landingpage/LP_TwinCard.jsx
+++ src/Components/Landingpage/LP_TwinCard.jsx
@@ -1,4 +1,4 @@
-const HTwinCard = ({ children }) => {
+const LP_TwinCard = ({children}) => {
   return (
     <section className="w-[88%] z-10 flex items-center justify-around gap-7 mt-[82px] mb-[57.8px]">
       {children}
@@ -6,4 +6,4 @@ const HTwinCard = ({ children }) => {
   )
 }
 
-export default HTwinCard
+export default LP_TwinCard
