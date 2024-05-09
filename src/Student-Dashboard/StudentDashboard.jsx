0435f53ffc7a03e2767b2950f852eba452a0d2a4
boma <94929211+Bomagift@users.noreply.github.com>
2024-05-09 16:12:58 -0400

    still working

--- src/Student-Dashboard/StudentDashboard.jsx
+++ src/Student-Dashboard/StudentDashboard.jsx
@@ -1,32 +1,43 @@
-import './StudentDashBoard.css'
+import "./StudentDashBoard.css";
 
 function StudentDashboard() {
   return (
     <div className="container">
-    <div className="header">
-      <div className="searchBox">
-        <i className="fa fa-search"></i> Search
-      </div>
-      <div className="userSection">
-        <i className="fa fa-bell"></i>
-        <img className="studentPhoto" src="src/Images/LandingPage/businessman-working-laptop 1@2x.png" alt="Student" />
-        <div className="userInfo">
-          <div className="userName">Oghenemarho</div>
-          <div className="userType">Student</div>
+      <div className="header">
+        <div className="searchBox">
+          <i className="fa fa-search"></i> Search
         </div>
-        <i className="fa fa-ellipsis-h"></i>
+        <div className="userSection">
+          <i className="fa fa-bell"></i>
+          <img
+            className="studentPhoto"
+            src="src/Images/LandingPage/businessman-working-laptop 1@2x.png"
+            alt="Student"
+          />
+          <div className="userInfo">
+            <div className="userName">Oghenemarho</div>
+            <div className="userType">Student</div>
+          </div>
+          <i className="fa fa-ellipsis-h"></i>
+        </div>
       </div>
-    </div>
 
-    <div className="bluebox">
-    <div className="blueboxcontent">
-    <div className="naMe">Hello, <span className="orange">Oghenemarho</span></div>
-            <div className="learn">Let’s learn something new today!</div>
-            <div>You have completed 0% of your course</div>
-            <div>Please check your course.</div>
-            <div className="calendar"><i className="calendarIcon far fa-calendar-alt"></i> May</div>
-            </div>
-            </div>
+      <div className="bluebox">
+        <div className="blueboxcontent">
+          <div className="naMe">
+            Hello, <span className="orange">Oghenemarho</span>
+          </div>
+          <div className="learn">Let’s learn something new today!</div>
+          <div className="course">
+            You have completed 0% of your course Please check your course.
+          </div>
+          <div className="course">Please check your course.</div>
+
+          <div className="calendar">
+            <i className="calendarIcon far fa-calendar-alt"></i>
+          </div>
+        </div>
+      </div>
 
       <div className="statistics">
         <div className="statBox">
@@ -41,20 +52,23 @@ function StudentDashboard() {
           <div className="statTitle">Completed Quizzes</div>
           <div className="statNumber">0</div>
         </div>
-        <div className="paymentHistory">
-          <div className="paymentTitle">Payment History <span className="seeAll">See All</span></div>
+
+        <div className="side">
+          <div className="paymentHistory">
+            <div className="paymentTitle">
+              Payment History <span className="seeAll">See All</span>
+            </div>
+          </div>
+          <div className="productDesign">
+            <div className="productInfo">
+              <div className="productName">Product Design</div>
+              <div className="productPrice">₦10,000 </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="productDesign">
-        <div className="productInfo">
-          <div className="productName">Product Design</div>
-          <div className="productPrice">₦10,000 CBT</div>
-        </div>
-    </div>
-
     </div>
   );
 }
 
-
-export default StudentDashboard 
+export default StudentDashboard;
