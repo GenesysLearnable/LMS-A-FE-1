8b1832543f9d997e7a57c704d69fe4fbf53f652d
COJUrama <uramacoju@gmail.com>
2024-05-11 09:57:59 +0100

    Added the lessons in the class.

---
+++ src/Components/Courses/ClassLessons.jsx
@@ -0,0 +1,50 @@
+import IntroductoryLesson from './IntroductoryLesson';
+import OtherLessons from './OtherLessons';
+import LessonCoreDetails from './LessonCoreDetails';
+import Style from './ClassLessons.module.css';
+
+const ClassLessons = function () {
+  return (
+    <div className={Style.lessons}>
+      <LessonCoreDetails numberOfLessons={5} totalDuration="16min 14secs" />
+
+      <div>
+        <IntroductoryLesson
+          title="Getting started in Product design"
+          minutes={1}
+          seconds={26}
+        />
+      </div>
+
+      <OtherLessons
+        number={2}
+        title="Introduction to User experience"
+        minutes={1}
+        seconds="00"
+      />
+
+      <OtherLessons
+        number={3}
+        title="Introduction to Figma"
+        minutes={4}
+        seconds="09"
+      />
+
+      <OtherLessons
+        number={4}
+        title="Introduction to User interface design"
+        minutes={2}
+        seconds="54"
+      />
+
+      <OtherLessons
+        number={5}
+        title="Responsive designs"
+        minutes={1}
+        seconds="33"
+      />
+    </div>
+  );
+};
+
+export default ClassLessons;
---
+++ src/Components/Courses/IntroductoryLesson.jsx
@@ -0,0 +1,20 @@
+import Style from './IntroductoryLesson.module.css';
+
+const IntroductoryLesson = function ({ title, minutes, seconds }) {
+  return (
+    <section className={Style.intro}>
+      <div className={Style.description}>
+        <img src="/src/Images/Courses/Play-Icon.svg" alt="Play" />
+
+        <h4>1.</h4>
+
+        <h4>{title}</h4>
+      </div>
+      <h4>
+        {minutes}:{seconds}
+      </h4>
+    </section>
+  );
+};
+
+export default IntroductoryLesson;
