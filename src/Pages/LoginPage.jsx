c90c59a79eddd995b75708f58f3ed66b304ac5f5
Mereson <chimeremnmaojinta@gmail.com>
2024-05-12 23:22:18 +0100

    implemented login logic

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,34 +3,56 @@ import CASPageLayout from "../Components/Signup and Login/CASPageLayout"
 import FormCard from "../Components/Signup and Login/FormCard"
 import Input from "../Components/Signup and Login/Input"
 import ButtonFeature from "../Components/Signup and Login/ButtonFeature"
+import useFetch from "../utlis/useFetch"
 
 const LoginPage = () => {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [loading, setLoading] = useState(false)
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  })
 
-  const handleSubmit = () => {}
+  const fetchData = useFetch()
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormValues((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleSubmit = async () => {
+    setLoading(true)
+    try {
+      let res = await fetchData.post("login", formValues)
+      console.log(res)
+      return
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
 
   return (
     <CASPageLayout>
-      <FormCard text={"Login"} action={handleSubmit}>
+      <FormCard text={"Login"}>
         <div className="flex flex-col gap-3">
           <Input
-            type={"text"}
+            name="email"
+            type="email"
             placeholder={"Email address"}
-            value={email}
-            setValue={setEmail}
+            value={formValues.email}
+            onChange={handleChange}
           />
           <Input
+            name="password"
             type={"password"}
             placeholder={"Password"}
-            value={password}
-            setValue={setPassword}
+            value={formValues.password}
+            onChange={handleChange}
           />
           <ButtonFeature
             text={"Don't have an account?"}
             buttonText={"Login"}
             spanText={" Create account"}
-            path={"/signup"}
+            path={""}
+            onClick={handleSubmit}
           />
         </div>
       </FormCard>
