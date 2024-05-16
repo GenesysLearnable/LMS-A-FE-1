ba4adb0d0a1da22a359ba4f93c0ddf98eb636651
Mereson <chimeremnmaojinta@gmail.com>
2024-05-16 10:52:53 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ import Account from "./Pages/student pages/Account"
 import StudentCourses from "./Pages/student pages/StudentCourses"
 import Forum from "./Pages/student pages/Forum"
 import Certification from "./Pages/student pages/Certification"
-import Course from "./Components/Landingpage/Course"
 import CourseContent from "./Components/Dashboard_courses/CourseContent"
 // Routes
 const router = createBrowserRouter(
