8387719fc4144d856033981d5d460a30876aba6c
COJUrama <uramacoju@gmail.com>
2024-05-24 13:55:31 +0100

    Rectifying the Image SIze Problem.

--- src/Components/Courses/Course/CourseDetails.jsx
+++ src/Components/Courses/Course/CourseDetails.jsx
@@ -9,7 +9,7 @@ export const details = [
   },
   {
     id: "2",
-    img: "/Images/Courses/Frontend-Course.svg",
+    img: "/Images/Courses/Main-Frontend-Course.png",
     title: "Frontend",
     text: "Embark on a journey into the world of product design. Begin by exploring the fundamentals, understanding the principles that shape exceptional designs.",
     students: "1,200",
@@ -17,7 +17,7 @@ export const details = [
   },
   {
     id: "3",
-    img: "/Images/Courses/Backend-Course.svg",
+    img: "/Images/Courses/Main-Backend-Course.png",
     title: "Backend",
     text: "Embark on a journey into the world of product design. Begin by exploring the fundamentals, understanding the principles that shape exceptional designs.",
     students: "1,200",
@@ -25,7 +25,7 @@ export const details = [
   },
   {
     id: "4",
-    img: "/Images/Courses/Web3-Course.svg",
+    img: "/Images/Courses/Main-Web3-Course.png",
     title: "Web3",
     text: "Embark on a journey into the world of product design. Begin by exploring the fundamentals, understanding the principles that shape exceptional designs.",
     students: "1,200",
